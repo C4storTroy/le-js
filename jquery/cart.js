27964cf0ade6a5c7ed7dc967adc77c31620b4ff0
Lucio Moraes <luciomoraes@gmail.com>
2016-12-19 14:33:40 +1300

    play with selectors

--- jquery/cart.js
+++ jquery/cart.js
@@ -1,16 +1,37 @@
-var updateData = function(){
-  var items = $(".item-total");
-  var total = 0;
-  for (var i=0;i < items.length; i++){
-    var item = $(items[i]);
-    var value = parseFloat(item.text());
-    total = total +value;
-  }
-
-  $("#total-value").text(total);
-  $("#quantity-items").text(items.length);
+var oneAd = function(){
+  var ads=["What do you think about a new car?",
+           "What do you think about a new bike?",
+           "What do you thinkg about a new guitar?",
+           "What do you thinkg about a new TV?"];
+  var posAd = Math.floor(ads.length * Math.random());
+  var text = ads[posAd];
+  var tr = $("<tr>").addClass("adv").append("<td>");
+  tr.find("td").attr("colspan",6).text(text);
+  return tr;
 };
 
+/**
+function to update page
+**/
+var updateData = function(){
+  var car = $(".cart");
+  car.each(function(){
+    var car = $(this);
+    var items = car.find(".item-total:visible");
+    var total = 0;
+    for (var i=0;i < items.length; i++){
+      var item = $(items[i]);
+      var value = parseFloat(item.text());
+      total = total +value;
+    }
+    car.find(".total-value").text(total);
+    car.find(".quantity-items").text(items.length);
+  });
+};
+
+/**
+function to remove items
+**/
 var removeItem = function(event){
   event.preventDefault();
 
@@ -30,17 +51,52 @@ var removeItem = function(event){
 
 };
 
+/**
+function to back removed items
+**/
 var undo = function(){
-  $("tr:visible").removeClass("recovered");
-  var trs = $("tr:hidden");
-  trs.addClass("recovered").show();
+  var car = $(this).closest('.cart');
+
+  car.find('tr:visible').removeClass('recovered');
+
+  var trs = car.find('tr:hidden');
+  trs.addClass("recovered");
+  trs.show();
+  updateData();
   /**trs.addClass("recovered");
   trs.show();**/
 };
 
+var giveBright = function(){
+  $(this).find(".remove-item").fadeIn();
+  $(this).addClass("hovering");
+};
+var delBright = function(){
+  $(this).find(".remove-item").fadeOut();
+  $(this).removeClass("hovering");
+};
+
+var alterAd = function(event){
+  event.preventDefault();
+  $(".adv").fadeToggle();
+  $(".alter-ad").toggle();
+};
+
+
+
 var afterLoad = function(){
   updateData();
-  $("#undo").click(undo);
+  $(".undo").click(undo);
   $(".remove-item").click(removeItem);
+  $("tr:nth-child(3n)").each(function(){
+    oneAd().insertAfter($(this));
+  });
+  $("tbody tr").hover(giveBright, delBright);
+  /**$("#hide-ad").click(alterAd);
+  $("#show-ad").click(alterAd);**/
+  $(".alter-ad").click(alterAd);
+  //$("tr").on("mouseenter", giveBright);
+  //$("tr").on("mouseleave", delBright);
 };
 $(afterLoad);
+//afterLoad();
